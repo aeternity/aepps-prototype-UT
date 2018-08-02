0716a5e29e85fdd65a099e4721e6564d8667ed35
SamLife91 <sbarguti@quanterall.com>
2018-08-02 11:26:31 +0300

    - add standalone cards components

---
+++ src/components/cardBack.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="card-back">
+    <div class="row main">
+      <div class="item">
+        <img src="@/assets/icn/barcode.svg" alt="">
+      </div>
+      <div class="item chunked">
+        <span v-for="chunk in chunkAddress" :key="chunk">
+          {{chunk}}
+        </span>
+
+      </div>
+    </div>
+    <div class="row actions">
+      <div class="item"></div>
+      <div class="item">
+        <img src="@/assets/icn/copy-light.svg" alt="">
+        copy
+      </div>
+      <div class="item">
+        <img src="@/assets/icn/share-light.svg" alt="">
+        share
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    account: {
+      type: Object,
+      default: function () {
+        return {
+          name: 'Daily',
+          address: 'ak$G2CCeMjQffK5K21lIun3GzAuN13vhAfcKBrUPSKhSQ8RcgHP1e',
+          balance: 0,
+          words: 'test test test',
+          unit: 'AE'
+        }
+      }
+    }
+  },
+  computed: {
+    chunkAddress () {
+      return this.account.address.match(/.{1,3}/g)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-back {
+  background: hsl(210, 33%, 19%);
+  max-height: 192px;
+  max-width: 312px;
+  color: #f8f8f8;
+  margin: 0 auto;
+  font-size: 17px;
+  border-radius: 8px;
+  .row {
+    display: flex;
+    .item {
+      flex:1;
+    }
+    .chunked {
+      padding-top: 4%;
+      line-height: 1em;
+      display: grid;
+      grid-template-columns: 33.3% 33.3% 33.3%;
+      font-family: 'aeternity mono';
+      span {
+        text-align: left;
+      }
+    }
+    &.main .item:nth-child(2) {
+      font-weight: 500;
+      letter-spacing: 2.1px;
+    }
+    &.main img {
+      transform: scale(1.1);
+    }
+  }
+  .actions {
+    background-color: #001833;
+    padding: 8px 0px;
+    font-size: 11px;
+    font-weight: 500;
+    text-transform: uppercase;
+    position: relative;
+    bottom: 10px;
+    border-radius: 0 0 8px 8px;
+    img {
+      height: 14px;
+      width: 14px;
+    }
+    .item:nth-child(1) {
+      flex:2;
+    }
+  }
+}
+</style>
---
+++ src/components/cardFront.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="card-front">
+    <div class="row heading">
+      <div class="item">
+        <ae-identity-avatar class="avatar" :address="account.address"></ae-identity-avatar>
+      </div>
+      <div class="item">
+        {{account.name}}
+      </div>
+      <div class="item">
+        {{`${account.balance} ${account.unit}`}}
+      </div>
+    </div>
+    <div class="row address">
+      <div class="item card-secure-label">
+        secure <br> label
+      </div>
+      <div class="item">
+        {{account.address.slice(0,3)}}
+        {{account.address.slice(3,6)}}
+        {{account.address.slice(6,9)}}
+        <p>
+          ··· ··· ···
+        </p>
+        {{account.address.slice(-3)}}
+        {{account.address.slice(-6,-3)}}
+        {{account.address.slice(-9,-6)}}
+      </div>
+    </div>
+    <div class="row actions">
+      <div class="item">
+        <img src="@/assets/icn/edit-light.svg" alt="">
+        rename
+      </div>
+      <div class="item">
+        <img src="@/assets/icn/copy-light.svg" alt="">
+        copy
+      </div>
+      <div class="item">
+        <img src="@/assets/icn/share-light.svg" alt="">
+        share
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { AeIdentityAvatar } from '@aeternity/aepp-components'
+export default {
+  components: {
+    AeIdentityAvatar
+  },
+  props: {
+    account: {
+      type: Object,
+      default: function () {
+        return {
+          name: 'Daily',
+          address: '0x2x3121231230x2x312123123',
+          balance: 0,
+          words: 'test test test',
+          unit: 'AE'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-front {
+  max-width: 312px;
+  // max-height: 192px;
+  margin: 0 auto;
+  background: #ff0d6a;
+  color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 0 16px 0px rgba(26,68,121,.2);
+  .row {
+    display: flex;
+    align-items: center;
+    font-size: 17px;
+    &.heading {
+      .item {
+        flex:2;
+        .avatar {
+          width: 32px;
+          height: 32px;
+        }
+      }
+      .item:first-child {
+        flex:1;
+      }
+      .item:nth-child(2) {
+        text-align: left;
+        font-weight: 500;
+      }
+    }
+    &.actions {
+      .item {
+        display: flex;
+      }
+      .item:first-child {
+        flex:2;
+      }
+    }
+    &.address {
+      padding: 40px 10px 8px;
+      font-weight: 700;
+      .item {
+        color: #c02967;
+        letter-spacing: 1.9px;
+        p {
+          margin: 0;
+          letter-spacing: 4.5px;
+        }
+      }
+      .item:nth-child(2) {
+        font-family: 'aeternity mono';
+        p {
+          letter-spacing: 1.1px;
+        }
+      }
+    }
+    .item {
+      flex:1;
+      padding: 5px 0 0;
+    }
+    &.actions {
+      background: rgba(0,0,0,.2);
+      padding: 9px;
+      font-size: 11px;
+      text-transform: uppercase;
+      letter-spacing: 1.1px;
+      img {
+        height: 14px;
+        width: 14px;
+        margin-right: 5px;
+      }
+    }
+    .card-secure-label {
+      align-self: flex-end;
+      text-align: left;
+      text-transform: uppercase;
+      font-size: 11px !important;
+      letter-spacing: 1.1px;
+    }
+  }
+}
+</style>
