ddd5343842be560a6fcf00c355714bb6bc6ceae7
SamLife91 <sbarguti@quanterall.com>
2018-08-02 11:28:52 +0300

    add settings pages { settings, language, network, remote }

---
+++ src/pages/settings/SettingsLang.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="settings-lang">
+    <Header>
+      <template slot="back-arrow">
+        <router-link to="settings">
+          <img src="@/assets/icn/back.svg" alt="">
+        </router-link>
+      </template>
+      <template slot="page-name">
+        {{this.$route.name}}
+      </template>
+      <template slot="nav">
+        <router-link to="settings">
+          <img src="@/assets/icn/settings.svg" alt="">
+        </router-link>
+        <router-link to="/">
+          <img src="@/assets/icn/list.svg" alt="">
+        </router-link>
+        <router-link class="avatar" to="accounts">
+          <ae-identity-avatar address='ak$G2CCeMjQffK5K21lIun3GzAuN13vhAfcKBrUPSKhSQ8RcgHP1e'>
+          </ae-identity-avatar>
+        </router-link>
+      </template>
+    </Header>
+    <div class="container">
+      <div class="row">
+        <div class="content">
+          <h3>
+            English
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang" :checked="true"></radio>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content">
+          <h3>
+            German
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang"></radio>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content">
+          <h3>
+            Bulgarian
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang"></radio>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content">
+          <h3>
+            Chinese
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang"></radio>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content">
+          <h3>
+            Japanese
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang"></radio>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content">
+          <h3>
+            Korean
+          </h3>
+        </div>
+        <div class="radio">
+          <radio name="lang"></radio>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import radio from '@/components/Radio'
+import Header from '@/components/Header'
+import { AeIdentityAvatar } from '@aeternity/aepp-components'
+
+export default {
+  components: {
+    radio, Header, AeIdentityAvatar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.settings-lang .container {
+  width: 85vw;
+  margin: 0 auto;
+}
+.settings-lang {
+  .row {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #edf3f7;
+    .content {
+      padding: 28px 0;
+      flex:3;
+      text-align:left;
+      h3,p {
+        margin: 0;
+      }
+    }
+    .radio {
+      flex:1;
+      label {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+      }
+    }
+  }
+  .row:last-child {
+    border-bottom: 0;
+  }
+}
+.avatar {
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        div {
+          width: 24px;
+          height: 24px;
+        }
+  }
+</style>
