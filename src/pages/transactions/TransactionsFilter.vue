1442b696afc295106f8c565eb392728031f26eba
stilyana <stiliana.runtashka@gmail.com>
2018-08-17 14:38:18 +0300

    fixed lint warnings

--- src/pages/transactions/TransactionsFilter.vue
+++ src/pages/transactions/TransactionsFilter.vue
@@ -59,18 +59,18 @@
             </select>
           </div>
           <p class="text">
-             Type ak$ address or AENS name, or choose one of your accounts …
+            Type ak$ address or AENS name, or choose one of your accounts …
           </p>
           <div class="row__btn">
-      <div>
-        <img src="@/assets/icn/share.svg" alt=""> Accounts
-      </div>
-      <div>
-        <img src="@/assets/icn/share.svg" alt=""> Paste
-      </div>
-      <div>
-        <img src="@/assets/icn/share.svg" alt=""> Scan
-      </div>
+            <div>
+              <img src="@/assets/icn/share.svg" alt=""> Accounts
+            </div>
+            <div>
+              <img src="@/assets/icn/share.svg" alt=""> Paste
+            </div>
+            <div>
+              <img src="@/assets/icn/share.svg" alt=""> Scan
+            </div>
           </div>
         </div>
         <div class="item">
@@ -87,72 +87,74 @@
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 <script>
-  import Header from "@/components/Header";
-  export default {
-    data(){
-      return {
-        isBrowseEnable: false,
-        isBrowserMin: false,
-        selectedSort: 'Data(descending)',
-        selectedDirection:'Outgoing Transactions',
-        firstSelectedRange: 'From',
-        secondSelectedRange: 'To',
-        selectedAddress: 'Select',
-        sortOptions:[
-          {text: 'Data(descending)',value: 'Data(descending)'},
-          {text: 'Data(ascending)', value: 'Data(ascending)'}
-        ],
-        directionOptions:[
-          {text: 'Outgoing Transactions', value: 'Outgoing Transactions'},
-          {text: 'Incoming Transactions', value: 'Incoming Transactions'
-          }
-        ],
-        firstRangeOptions:[
-          {text:'From', value: 'From'},
-          {text: 'To', value: 'To'}
-        ],
-        secondRangeOptions: [
-          { text: 'From', value: 'From' },
-          { text: 'To', value: 'To' }
-        ],
-        addressOptions:[
-          { text: 'Select', value: 'Select' },
-          { text: 'A', value: 'A'}
-        ]
-      }
-    },
-    components: {
-      Header
+import Header from '@/components/Header'
+export default {
+  data () {
+    return {
+      isBrowseEnable: false,
+      isBrowserMin: false,
+      selectedSort: 'Data(descending)',
+      selectedDirection: 'Outgoing Transactions',
+      firstSelectedRange: 'From',
+      secondSelectedRange: 'To',
+      selectedAddress: 'Select',
+      sortOptions: [
+        { text: 'Data(descending)', value: 'Data(descending)' },
+        { text: 'Data(ascending)', value: 'Data(ascending)' }
+      ],
+      directionOptions: [
+        { text: 'Outgoing Transactions', value: 'Outgoing Transactions' },
+        {text: 'Incoming Transactions', value: 'Incoming Transactions'}
+      ],
+      firstRangeOptions: [
+        { text: 'From', value: 'From' },
+        { text: 'To', value: 'To' }
+      ],
+      secondRangeOptions: [
+        { text: 'From', value: 'From' },
+        { text: 'To', value: 'To' }
+      ],
+      addressOptions: [
+        { text: 'Select', value: 'Select' },
+        { text: 'A', value: 'A' }
+      ]
     }
-  };
+  },
+  components: {
+    Header
+  }
+}
 </script>
 <style lang="scss" scoped>
   .container {
-    display:flex;
+    display: flex;
     justify-content: space-evenly;
     flex-direction: column;
     height: 85vh;
   }
+
   .item {
     margin: 0 auto;
     min-height: 10vh;
-    display:flex;
+    display: flex;
     flex-direction: column;
     text-align: left;
     justify-content: space-around;
-    width:80vw;
+    width: 80vw;
     background-color: #f7fafc;
   }
+
   .title {
-    font-size:0.8em;
+    font-size: 0.8em;
     line-height: 1em;
     letter-spacing: 0.3px;
     color: #76818c;
     margin: 0.5em 0 0 1em;
   }
+
   .select {
     border: 0;
     background: transparent;
@@ -168,33 +170,39 @@
       border: solid 2px #f7fafc;
     }
   }
+
   .wrapper {
-    display:flex;
+    display: flex;
     margin: 0 auto;
     height: 10vh;
-    width:80vw;
+    width: 80vw;
     justify-content: space-between;
   }
-  .itemRow{
-    display:flex;
+
+  .itemRow {
+    display: flex;
     width: 40%;
     justify-content: space-between;
     background-color: #f7fafc;
   }
-  .row{
-    display:flex;
+
+  .row {
+    display: flex;
     justify-content: space-between;
   }
-  .row__btn{
-    display:flex;
+
+  .row__btn {
+    display: flex;
     justify-content: space-between;
     background-color: #edf3f7;
   }
-  .text{
+
+  .text {
     line-height: 1.5em;
-    color:#76818c;
+    color: #76818c;
     line-height: 1.41;
   }
+
   .btn {
     text-transform: uppercase;
     height: 8vh;
@@ -203,29 +211,29 @@
     box-shadow: 0 0 16px 0 rgba(27, 68, 121, 0.15);
     display: flex;
     justify-content: center;
-}
+  }
 
-.btn__link {
-  text-decoration: none;
-  padding-top: 20px;
-  height: 16px;
-  font-size: 13px;
-  font-weight: 900;
-  line-height: 1.23;
-  letter-spacing: 1.3px;
-  color: #ffffff;
-}
+  .btn__link {
+    text-decoration: none;
+    padding-top: 20px;
+    height: 16px;
+    font-size: 13px;
+    font-weight: 900;
+    line-height: 1.23;
+    letter-spacing: 1.3px;
+    color: #ffffff;
+  }
 
-.btn__inactive{
-  text-transform: uppercase;
-  font-weight: bold;
-  display:flex;
-  justify-content: center;
-}
+  .btn__inactive {
+    text-transform: uppercase;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+  }
 
-.btn__inactive__link{
-  margin-top:2vh;
-  text-decoration: none;
-  color: #203040;
-}
+  .btn__inactive__link {
+    margin-top: 2vh;
+    text-decoration: none;
+    color: #203040;
+  }
 </style>
